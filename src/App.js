79a7bef978779af0b138747d8a983c0fc3845bb2
1011497938 <1011497938@qq.com>
2019-05-08 11:18:20 +0800

    使用了semantic-ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import Test from './component/Test'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import 'semantic-ui-css/semantic.min.css'
 import Nav from './component/Nav'
 import { autorun } from 'mobx';
 import stateManger from './dataManager/stateManager';
@@ -12,9 +13,14 @@ class App extends React.Component{
 
   render(){
     return (
-      <div>
-        <Nav/>
-        <Test/>
+      <div style={{position: 'absolute'}}>
+        <div style={{position: 'absolute', height:'100%', top: 0, left: 0}}>
+          <Nav/>
+        </div>
+        <div style={{position: 'absolute', top: 0, left: 270}}>
+          <Test/>
+        </div>
+        {/*  */}
       </div>
     )
   }
--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { Grid, Menu, Segment } from 'semantic-ui-react'
 import stateManger from '../dataManager/stateManager';
 
-export default class Nav extends React.Component{
+export default class MenuExampleTabularOnLeft extends Component {
     constructor(props){
         super(props)
         this.state = {
@@ -10,39 +11,31 @@ export default class Nav extends React.Component{
         this.view_names = ['全局视图', '协同生态视图', '载体及资源视图', '服务价值视图', '服务过程视图', '服务目标视图', '登录/注册']
     }
 
-    render(){
-        const {selected_view} = this.state
-        const {view_names} = this
 
-        const renderViewBar = (text) => {
-            const handleClick = ()=>{
-                console.log('click',text)
-                stateManger.show_view_name.set(text)
-            }
-            return (
-            <li className={"nav-item " + (selected_view===text?"active":"")}>
-                <a className="nav-link" href="#" onClick={handleClick}>
-                    {text}
-                <span className="sr-only">(current)</span></a>
-            </li>                
-            )
-        }
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { selected_view } = this.state
 
         return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <span class="navbar-brand mb-0 h1">服务模式</span>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    {
-                        view_names.map(text=> renderViewBar(text))
+            <Menu pointing vertical>
+            {
+                this.view_names.map(text=>{
+                    const handleClick = ()=>{
+                        console.log('click',text)
+                        stateManger.show_view_name.set(text)
+                        this.setState({selected_view: text})
                     }
-                </ul>
-            </div>
-        </nav>
+                    return (
+                    <Menu.Item
+                        name={text}
+                        active={selected_view === text}
+                        onClick={handleClick}
+                    />
+                    )
+                })
+            }
+            </Menu>
         )
     }
 }

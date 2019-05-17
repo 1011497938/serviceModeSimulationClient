7696764c7cce74d26affbab7cfa93e32cb464356
monica309673 <1285098441@qq.com>
2019-05-17 18:12:18 +0800

    战略目标表单

---
+++ src/component/ui_components/StrategicGoalFormEdit.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import { Input, Menu, Table, Label, Icon,Form} from 'semantic-ui-react'
+import stateManger from '../../dataManager/stateManager';
+import { autorun } from 'mobx';
+
+export default class StrategicGoalFormEdit extends Component{
+    constructor(props){
+        super(props)
+         this.state={
+                    select_component: stateManger.selet_component_ingo.get()
+                }
+    }
+
+    render(){
+        return(
+            <div>
+                 <div>
+                    {this.state.select_component}
+                 </div>
+                 <form class='ui form'>
+                     <div>
+                        <h3 class="ui dividing header">战略目标属性编辑</h3>
+                     </div>
+
+                     <div class="field">
+                        <h4> 战略目标基础信息</h4>
+                     </div>
+
+                      <div class="two  fields">
+                            <div class="field">
+                               <label>战略目标名称</label>
+                               <input type="text"  placeholder="StrategicGoalName" />
+                            </div>
+
+                             <div class="field">
+                               <label>战略目标ID</label>
+                               <input type="text"  placeholder="StrategicGoalID" />
+                            </div>
+                      </div>
+
+                      <div class="inline fields">
+                          <label>完成主体</label>
+                          <div class="field">
+                            <div class="ui radio checkbox">
+                              <input type="radio" name="frequency"  />
+                              <label>主体1</label>
+                            </div>
+                          </div>
+                          <div class="field">
+                            <div class="ui radio checkbox">
+                              <input type="radio" name="frequency" />
+                              <label>主体2</label>
+                            </div>
+                          </div>
+                          <div class="field">
+                            <div class="ui radio checkbox">
+                              <input type="radio" name="frequency" />
+                              <label>主体3</label>
+                            </div>
+                          </div>
+                          <div class="field">
+                            <div class="ui radio checkbox">
+                              <input type="radio" name="frequency" />
+                              <label>主体4</label>
+                            </div>
+                          </div>
+                      </div>
+
+                       <div class="field">
+                          <label>简要介绍</label>
+                          <input type="text"  placeholder="Description" />
+                       </div>
+
+                        <div class="field">
+                             <label>预期完成时间</label>
+                             <input type="text"  placeholder="Description" />
+                       </div>
+
+
+                    <div class="ui button" tabindex="0">提交</div>
+                 </form>
+            </div>
+
+        )
+    }
+    }
+
+

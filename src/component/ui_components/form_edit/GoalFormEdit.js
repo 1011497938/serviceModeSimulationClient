e69e83256e2a9191e272b37a5fa1cc75801e2ac2
chenjintao <1285098441@qq.com>
2019-05-29 00:50:44 +0800

    目标视图表单

--- src/component/ui_components/form_edit/GoalFormEdit.js
+++ src/component/ui_components/form_edit/GoalFormEdit.js
@@ -1,91 +1,123 @@
 import React, { Component } from 'react'
-import { Input, Menu, Table, Label, Icon,Form} from 'semantic-ui-react'
-import stateManger from '../../../dataManager/stateManager';
-import { autorun } from 'mobx';
+import { Tab} from 'semantic-ui-react'
+import GoalBasicInfo from './GoalBasicInfo'
+import GoalComputeInfo from './GoalComputeInfo'
+import GoalConstraintInfo from './GoalConstraintInfo'
+import CustomizeInfo from './CustomizeInfo'
 
-export default class GoalFormEdit extends Component{
-    constructor(props){
-        super(props)
-        this.state={
-            select_component: stateManger.selet_component_ingo.get()
-        }
-    }
+const panes = [
+    { menuItem: '描述', render: () => <Tab.Pane ><GoalBasicInfo/></Tab.Pane> },
+    { menuItem: '计算', render: () => <Tab.Pane><GoalComputeInfo/></Tab.Pane> },
+    { menuItem: '约束', render: () => <Tab.Pane><GoalConstraintInfo/></Tab.Pane> },
+    { menuItem: '自定义', render: () => <Tab.Pane><CustomizeInfo/></Tab.Pane> }
+]
 
-    render(){
-        return(
-            <div>
-                 <div>
-                    {this.state.select_component}
-                 </div>
-                 <form class='ui form'>
-                     <div>
-                        <h3 class="ui dividing header">目标属性编辑</h3>
-                     </div>
+const GoalFormEdit = () =>( <Tab panes={panes} />)
 
-                     <div class="field">
-                        <h4> 目标基础信息</h4>
-                     </div>
-
-                      <div class="two  fields">
-                            <div class="field">
-                               <label>目标名称</label>
-                               <input type="text"  placeholder="GoalName" />
-                            </div>
-
-                             <div class="field">
-                               <label>目标ID</label>
-                               <input type="text"  placeholder="StrategicGoalID" />
-                            </div>
-                      </div>
+export default GoalFormEdit
 
 
-                          <label>完成主体</label>
-                          <div class="inline fields">
-                              <div class="field">
-                            <div class="ui radio checkbox">
-                              <input type="radio" name="frequency"  />
-                              <label>主体1</label>
-                            </div>
-                          </div>
-                          <div class="field">
-                            <div class="ui radio checkbox">
-                              <input type="radio" name="frequency" />
-                              <label>主体2</label>
-                            </div>
-                          </div>
-                          <div class="field">
-                            <div class="ui radio checkbox">
-                              <input type="radio" name="frequency" />
-                              <label>主体3</label>
-                            </div>
-                          </div>
-
-                      </div>
-
-                       <div class="field">
-                          <label>简要介绍</label>
-                          <input type="text"  placeholder="Description" />
-                       </div>
-
-                        <div class="two fields">
-                             <div class="field">
-                                  <label>预期完成时间</label>
-                                  <input type="text"  placeholder="FinishTime" />
-                             </div>
-                             <div class="field">
-                                   <label>目标优先级</label>
-                                   <input type="text"  placeholder="priority" />
-                              </div>
-                        </div>
 
 
-                     
-                    <div class="ui button" tabindex="0">提交</div>
-                 </form>
-            </div>
-
-        )
-    }
-    }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react'
+// import { Input, Menu, Table, Label, Icon,Form} from 'semantic-ui-react'
+// import stateManger from '../../../dataManager/stateManager';
+// import { autorun } from 'mobx';
+//
+// export default class StrategicGoalFormEdit extends Component{
+//     constructor(props){
+//         super(props)
+//          this.state={
+//                     select_component: stateManger.selet_component_ingo.get()
+//                 }
+//     }
+//     render(){
+//         return(
+//             <div>
+//                  <div>
+//                     {this.state.select_component}
+//                  </div>
+//                  <form class='ui form'>
+//                      <div>
+//                         <h3 class="ui dividing header">战略目标属性编辑</h3>
+//                      </div>
+//
+//                      <div class="field">
+//                         <h4> 战略目标基础信息</h4>
+//                      </div>
+//
+//                       <div class="two  fields">
+//                             <div class="field">
+//                                <label>战略目标名称</label>
+//                                <input type="text"  placeholder="StrategicGoalName" />
+//                             </div>
+//
+//                              <div class="field">
+//                                <label>战略目标ID</label>
+//                                <input type="text"  placeholder="StrategicGoalID" />
+//                             </div>
+//                       </div>
+//
+//                       <div class="inline fields">
+//                           <label>完成主体</label>
+//                           <div class="field">
+//                             <div class="ui radio checkbox">
+//                               <input type="radio" name="frequency"  />
+//                               <label>主体1</label>
+//                             </div>
+//                           </div>
+//                           <div class="field">
+//                             <div class="ui radio checkbox">
+//                               <input type="radio" name="frequency" />
+//                               <label>主体2</label>
+//                             </div>
+//                           </div>
+//                           <div class="field">
+//                             <div class="ui radio checkbox">
+//                               <input type="radio" name="frequency" />
+//                               <label>主体3</label>
+//                             </div>
+//                           </div>
+//                           <div class="field">
+//                             <div class="ui radio checkbox">
+//                               <input type="radio" name="frequency" />
+//                               <label>主体4</label>
+//                             </div>
+//                           </div>
+//                       </div>
+//
+//                        <div class="field">
+//                           <label>简要介绍</label>
+//                           <input type="text"  placeholder="Description" />
+//                        </div>
+//
+//                         <div class="field">
+//                              <label>预期完成时间</label>
+//                              <input type="text"  placeholder="Description" />
+//                        </div>
+//
+//
+//                     <div class="ui button" tabindex="0">提交</div>
+//                  </form>
+//             </div>
+//
+//         )
+//     }
+//     }
+//
+//

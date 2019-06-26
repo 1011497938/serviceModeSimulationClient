04ad7ac093e6eec616e78a235af6d6e3373016f0
monica309673 <1285098441@qq.com>
2019-06-26 10:50:13 +0800

    图表分析

--- src/component/ui_components/DataVisualize.js
+++ src/component/ui_components/DataVisualize.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { Button} from 'semantic-ui-react'
-import { Pagination } from 'antd';
-import Nav from '../ui_components/Nav'
+import { Button} from 'semantic-ui-react';
+import Nav from '../ui_components/Nav';
 import {
     Router, Route,Link
-} from 'react-router-dom'
+} from 'react-router-dom';
+
+
+
+import Bar from '../chart_components/Bar';
+import Pie from '../chart_components/Pie';
+import Radar from '../chart_components/Radar';
+import Sunburst from '../chart_components/Sunburst';
+import Line from '../chart_components/Line';
+import Newchart from '../chart_components/Newchart';
+
 class DataVisualize extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
-			areas:['个人文件','最近修改','我的收藏'],
+			areas:['目标数据图表','价值数据图表'],
 			show:false,
 			index:0,
 			index2:-1,
-			fileNames:['111', '村淘2','村淘3','村淘4', '村淘5','村淘6'],
 		}
+		this.handleClick = this.handleClick.bind(this);
 	}
+
+	handleClick() {
+	    
+	  }
+
 	render(){		
 	
 		return (	
@@ -26,45 +40,45 @@ class DataVisualize extends React.Component{
 		        </div>		
 				<div className='wrapper'>
 					<div className='leftbox'>
-						<h2>个人文件</h2>
-						<Button primary>新建文件</Button>
+						<h2>图表分析</h2>
+						{/* <Button primary onClick={this.handClick}>新建图表</Button> */}
+						<div>
+							<Newchart/>
+						</div>
 						<ul>
 							{this.state.areas.map((item,index)=>{
 								return(
 		                            <li onClick={this.handleClickareas.bind(this,index)} className={this.state.index==index?"selected":""}>
-										<i className={index==0?"home icon":(index==1?"time icon":"star icon")}></i>
 										<span>{item}</span>
 									</li>
 							    )
 							})}
 						</ul>
 					</div>
-					<div className='rightbox'>
-					 <p style={{fontSize:'16px'}}>{this.state.areas[this.state.index]}</p>
-						<table>
-						  <thead>
-						    <tr>						    
-						      <th>文件名</th>
-						      <th>拥有者</th>
-						      <th>最后修改时间</th>
-						    </tr>
-						  </thead>
-						  	{this.state.fileNames.map((item,index)=>{
-								return(					 
-										<tbody  onClick={()=>{window.location.href="http://localhost:3000/"}} onMouseOver={this.handleClickfiles.bind(this,index)}>
-					                            <tr className={this.state.index2==index?"onfocusfile":""}>
-													<td>{this.state.fileNames[index]}</td>
-								              		<td>Sae</td>
-								      				<td>2019-6-24</td>
-												</tr>
-										</tbody>									
-							           )
-							       })}
-						</table>	
-						<Pagination className="page" size="small" hideOnSinglePage={true} total={50} />				
+					<div className='rightbox' >
+						<div style={{height:'40%',width:'80%'}}>
+			 				<div style={{height:'80%',float:'left',width:'30%'}}>
+								<Pie/>
+							</div>		
+							<div style={{height:'80%',width:'30%',float:'left'}}>
+								<Radar/>
+							</div>
+							<div style={{height:'80%',width:'30%',float:'left'}}>
+								<Sunburst/>
+							</div>
+						</div>
+			   			<div style={{height:'40%',width:'80%'}}>
+							<div style={{height:'80%',width:'50%',float:'left'}}>
+							  <Bar/>
+							</div>
+						    <div style={{height:'80%',width:'50%',float:'left'}}>
+							  <Line/>
+							</div>
+						</div>       
 					</div>
 				</div>
 			 </div>
+			
 			)
 
 	}
@@ -74,5 +88,9 @@ class DataVisualize extends React.Component{
 		handleClickfiles(index){
 	       this.setState({index2:index})
         }
-}      
+}     
+
+  
+//   AppRegistry.registerComponent('ReactNativeProject', () => ReactNativeProject);
+
 export default DataVisualize;
